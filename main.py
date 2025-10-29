e030301ea073ca53f69ac097b71ab5bb84f70b8b
kkk111386 <wongok.s240475@ggh.goe.go.kr>
2025-10-29 19:08:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas as pd
 # https://raw.githubusercontent.com/huhshin/streamlit/master/data_iris.csv 읽고 나타내기 
 def main_page():
     st.header('Main Page')
-    st.image('https://images.pexels.com/photos/5677011/pexels-photo-5677011.jpeg?auto=compress&cs=tinysrgb&w=200')
+    st.image('https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202505/19/511bcacb-d6c9-4cad-98ee-8ec5b91f58ce.jpg')
     iris = pd.read_csv('https://raw.githubusercontent.com/huhshin/streamlit/master/data_iris.csv')
     st.write(iris)
     
